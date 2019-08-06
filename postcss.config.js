8d1b0151e7b70c7570c9631b74a42de73e9afceb
Julian Fell <julesfell@gmail.com>
2019-08-06 12:13:21 +0200

    update deps and domain

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,11 @@
-
-var postcss = require('postcss')
-
-module.exports = {
-  use: [
-    'postcss-import',
-    'postcss-custom-media',
-    'postcss-custom-properties',
-    'postcss-calc',
-    'postcss-color-function',
-    'postcss-discard-comments',
-    'autoprefixer',
-  ],
-  input: 'src/base.css',
-  dir: 'css'
-}
-
-
+module.exports = ctx => ({
+  plugins: {
+    'postcss-import': {},
+    'postcss-custom-media': {},
+    'postcss-custom-properties': {},
+    'postcss-calc': {},
+    'postcss-color-function': {},
+    'postcss-discard-comments': {},
+    'autoprefixer': {}
+  }
+})
